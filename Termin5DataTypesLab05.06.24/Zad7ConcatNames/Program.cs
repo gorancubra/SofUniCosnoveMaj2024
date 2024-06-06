c4b44d86633eec74420f30b031e44b50c5504090
Goran <95975628+gorancubra@users.noreply.github.com>
2024-06-06 20:11:01 +0200

    stage4

---
+++ Termin5DataTypesLab05.06.24/Zad7ConcatNames/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad7ConcatNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{name1}{delimiter}{name2}");
+        }
+    }
+}

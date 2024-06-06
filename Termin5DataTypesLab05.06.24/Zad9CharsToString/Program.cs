73fa5e69deed6e7c4abcaff83ac5fa36cb95be38
Goran <95975628+gorancubra@users.noreply.github.com>
2024-06-06 20:00:37 +0200

    final commit

---
+++ Termin5DataTypesLab05.06.24/Zad9CharsToString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zad9CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+
+            string combined = first +""+ second + third;
+            
+            Console.WriteLine(combined);
+
+        }
+    }
+}
